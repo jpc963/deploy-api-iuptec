ed058d51898cd5900ea267c2a5dd5dfe1269178f
João Pedro <oonoreti@gmail.com>
2023-01-27 12:03:17 -0300

    test

--- iuptec/settings.py
+++ iuptec/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jr3@=4$6v0esavq4(p92p)0lyp-ea9$p#&=rla*)e&8l$gaxma'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.railway.app']
 
 
 # Application definition
